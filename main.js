6670af061950644cef93a281dae97b4791b207c0
Andrea Franchini <f.andrea1602@icloud.com>
2014-05-18 16:58:10 +0200

    Upload of the main code
    the home page is called “Clockswish.html”. Edit the Info.plist for
    change it. The other 3 files contain the scripts.

---
+++ main.js
@@ -0,0 +1,38 @@
+var clockswish = function(){
+var currentTime = new Date();
+var currentHours = currentTime.getHours();
+var currentMinutes = currentTime.getMinutes();
+//var currentSeconds = currentTime.getSeconds();
+if(currentHours < 10)
+{
+currentHours= "0" + currentHours.String;
+}
+if(currentMinutes < 10) 
+{
+currentMinutes="0" + currentMinutes;
+}
+
+var myName = currentHours +":"+ currentMinutes /*+":"+ currentSeconds*/;
+
+var red = [0, 100, 63];
+var orange = [40, 100, 60];
+var green = [75, 100, 40];
+var blue = [196, 77, 55];
+var purple = [280, 50, 60];
+var letterColors = [red, orange, green, blue, purple];
+
+drawName(myName, letterColors);
+
+if(10 < 3)
+{
+    bubbleShape = 'square';
+}
+else
+{
+    bubbleShape = 'circle';
+}
+
+bounceBubbles();
+};
+clockswish();
+//window.setInterval(clockswish(), 60000)
